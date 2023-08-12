2e2e0c7828f26586f80f141346c52f2abbe2761e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-08-12 22:46:07 +0200

    Revert "nixos: rebuild less derivations on nixpkgs/config hash changes."
    This reverts commit 82eea909c243a31075a04a84c934f4da1644c082.

--- nixos/modules/installer/tools/nixos-version.sh
+++ nixos/modules/installer/tools/nixos-version.sh
@@ -7,25 +7,25 @@ case "$1" in
     exit 1
     ;;
   --hash|--revision)
-    revision="$(jq --raw-output '.nixpkgsRevision // "null"' < @nixosVersionJson@)"
-    if [ "$revision" == "null" ]; then
+    if ! [[ @revision@ =~ ^[0-9a-f]+$ ]]; then
       echo "$0: Nixpkgs commit hash is unknown" >&2
       exit 1
     fi
-    echo "$revision"
+    echo "@revision@"
     ;;
   --configuration-revision)
-    revision="$(jq --raw-output '.configurationRevision // "null"' < @nixosVersionJson@)"
-    if [ "$revision" == "null" ]; then
+    if [[ "@configurationRevision@" =~ "@" ]]; then
       echo "$0: configuration revision is unknown" >&2
       exit 1
     fi
-    echo "$revision"
+    echo "@configurationRevision@"
     ;;
   --json)
-    cat @nixosVersionJson@
+    cat <<EOF
+@json@
+EOF
     ;;
   *)
-    jq --raw-output '.nixosVersion + " (" + .nixosCodeName + ")"' < @nixosVersionJson@
+    echo "@version@ (@codeName@)"
     ;;
 esac
